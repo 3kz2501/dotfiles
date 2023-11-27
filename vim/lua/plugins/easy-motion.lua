0139b18d44b726083a7fb8603404d814a295ac8e
nilnull <nilnull@pop-os.localdomain>
2023-11-27 22:07:46 +0900

    add easymotion

---
+++ vim/lua/plugins/easy-motion.lua
@@ -0,0 +1,10 @@
+return {
+  'easymotion/vim-easymotion',
+  keys = {
+    {"<leader>s","<Plug>(easymotion-bd-f2)", desc="easy-motion-db-f2",},
+    {"<leader>s","<Plug>(easymotion-overwin-f2)", desc="easy-motion-overwin-f2",mode = "n"},
+    {"<leader>l","<Plug>(easymotion-bd-jk)", desc="easy-motion-db-jk",},
+    {"<leader>l","<Plug>(easymotion-overwin-line)", desc="easy-motion-overwin-line", mode="n"},
+  },
+}
+
--- vim/lua/plugins/telescope.lua
+++ vim/lua/plugins/telescope.lua
@@ -3,5 +3,7 @@ return {
   keys = {
       { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
       { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "live grep" },
+      { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "help tag" },
     },
 }
