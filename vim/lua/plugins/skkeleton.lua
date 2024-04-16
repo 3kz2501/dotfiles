ac9934f24af21ca3430b07064c4bb341e7d019a6
nilnull <nilnull@pop-os.localdomain>
2024-04-17 00:22:38 +0900

    fix sskeleton

--- vim/lua/plugins/skkeleton.lua
+++ vim/lua/plugins/skkeleton.lua
@@ -1,27 +1,27 @@
-return  {
-    "vim-skk/skkeleton",
-    dependencies = {
-      "vim-denops/denops.vim",
-      "Shougo/ddc.vim",
-    },
-    keys = {
-      {"<C-j>", "<Plug>(skkeleton-toggle)", desc="toggle skkeleton", mode="i"},
-    },
-    config = function()
-      vim.fn['skkeleton#config']{
-        eggLikeNewline = true,
-        useSkkServer = true,
-        immediatelyCancel = false,
-        registerConvertResult = true,
-        globalDictionaries = {
-          "~/.skk/SKK-JISYO.L",
-          "~/.skk/SKK-JISYO.fullname",
-          "~/.skk/SKK-JISYO.geo",
-          "~/.skk/SKK-JISYO.jinmei",
-          "~/.skk/SKK-JISYO.propernoun",
-          "~/.skk/SKK-JISYO.station",
-        },
-       }
-    end,
-    event = "VeryLazy",
+return {
+	"vim-skk/skkeleton",
+	dependencies = {
+		"vim-denops/denops.vim",
+		"Shougo/ddc.vim",
+	},
+	keys = {
+		{ "<C-j>", "<Plug>(skkeleton-toggle)", desc = "toggle skkeleton", mode = "i" },
+	},
+	config = function()
+		vim.fn["skkeleton#config"]({
+			eggLikeNewline = true,
+			useSkkServer = true,
+			immediatelyCancel = false,
+			registerConvertResult = true,
+			globalDictionaries = {
+				vim.fn.expand("~/.skk/SKK-JISYO.L"),
+				vim.fn.expand("~/.skk/SKK-JISYO.fullname"),
+				vim.fn.expand("~/.skk/SKK-JISYO.geo"),
+				vim.fn.expand("~/.skk/SKK-JISYO.jinmei"),
+				vim.fn.expand("~/.skk/SKK-JISYO.propernoun"),
+				vim.fn.expand("~/.skk/SKK-JISYO.station"),
+			},
+		})
+	end,
+	event = "VeryLazy",
 }
