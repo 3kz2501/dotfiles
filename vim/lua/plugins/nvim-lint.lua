b15f6be8db93bc309755bc0a7c663b1e642ec59b
= <=>
2024-04-17 11:51:48 +0900

    fix some issue

--- vim/lua/plugins/nvim-lint.lua
+++ vim/lua/plugins/nvim-lint.lua
@@ -12,11 +12,14 @@ return {
       typescript = { "biome" },
       javascriptreact = { "biome" },
       typescriptreact = { "biome" },
+      vue = { "biome" },
       svelte = { "biome" },
       python = { "pylint" },
       sql = { "sqlfluff" },
-      jjson = { "jsonlint" },
+      json = { "jsonlint" },
       go = { "staticcheck" },
+      lua = { "luacheck" },
+      bash = { "shellcheck" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
